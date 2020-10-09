a2ed5eba299060d38482fe437fe76128b5abf060
bigsky-park <ptm302@gmail.com>
2020-10-09 22:53:53 +0900

    Add counter to hello api

--- api/v1/handler/hello.go
+++ api/v1/handler/hello.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "sample_app_hello_call_count",
+		Help: "The total number of hello call count",
+	})
 )
 
 type Hello struct {
@@ -18,6 +28,7 @@ func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.l.Println("Hello is called")
 
 	if r.Method == http.MethodGet {
+		opsProcessed.Inc()
 		fmt.Fprintf(rw, "Hello")
 	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
